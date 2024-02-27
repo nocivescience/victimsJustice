d3129e5bdfec55c160491a7fc48b3440df86e489
nocive.science <nocive.science@gmail.com>
2024-02-27 03:18:08 -0300

    cambié el color del botón SEARCH a rojo danger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,7 +34,7 @@ const NavBar = () => {
                         </ul>
                         <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                            <button className="btn btn-outline-danger" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
